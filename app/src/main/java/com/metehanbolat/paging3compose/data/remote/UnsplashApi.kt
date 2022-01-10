c45ba32e74ae591de30040c85aa375c37710d0d5
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-01-10 23:45:43 +0300

    Create UnsplashApi Interface

---
+++ app/src/main/java/com/metehanbolat/paging3compose/data/remote/UnsplashApi.kt
@@ -0,0 +1,25 @@
+package com.metehanbolat.paging3compose.data.remote
+
+import com.metehanbolat.paging3compose.BuildConfig
+import com.metehanbolat.paging3compose.model.UnsplashImage
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+interface UnsplashApi {
+
+    @Headers("Authorization: Client-ID ${BuildConfig.API_KEY}")
+    @GET("/photos")
+    suspend fun getAllImage(
+        @Query(value = "page") page: Int,
+        @Query(value = "per_page") per_page: Int
+    ): List<UnsplashImage>
+
+    @Headers("Authorization: Client-ID ${BuildConfig.API_KEY}")
+    @GET("/search/photos")
+    suspend fun searchImages(
+        @Query(value = "page") page: Int,
+        @Query(value = "per_page") per_page: Int
+    ): List<UnsplashImage>
+
+}
