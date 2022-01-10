9ccf6bc2a189af0fef7c8fdf7bd4bb01a6882a2d
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-01-10 23:49:06 +0300

    Setup Dagger-Hilt

--- app/src/main/java/com/metehanbolat/paging3compose/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/paging3compose/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.metehanbolat.paging3compose.ui.theme.Paging3ComposeTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
