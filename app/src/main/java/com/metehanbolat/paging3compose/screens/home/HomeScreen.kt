4cf4371155a37a65cae838f5b7de46b7c261bc7d
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-01-12 00:22:33 +0300

    Implement Home Screen

--- app/src/main/java/com/metehanbolat/paging3compose/screens/home/HomeScreen.kt
+++ app/src/main/java/com/metehanbolat/paging3compose/screens/home/HomeScreen.kt
@@ -1,9 +1,31 @@
 package com.metehanbolat.paging3compose.screens.home
 
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import androidx.paging.ExperimentalPagingApi
+import coil.annotation.ExperimentalCoilApi
+import com.metehanbolat.paging3compose.navigation.Screen
+import com.metehanbolat.paging3compose.screens.common.ListContent
 
+@ExperimentalCoilApi
+@ExperimentalPagingApi
 @Composable
-fun HomeScreen(navController: NavHostController) {
+fun HomeScreen(
+    navController: NavHostController,
+    homeViewModel: HomeViewModel = hiltViewModel()
+) {
+    val getAllImages = homeViewModel.getAllImages.collectAsLazyPagingItems()
 
+    Scaffold(
+        topBar = {
+            HomeTopBar(onSearchClicked = {
+                navController.navigate(Screen.Search.route)
+            })
+        },
+        content = {
+            ListContent(items = getAllImages)
+        }
+    )
 }
